c717fab4e50ee6b455e2f45fec80d283bf53d686
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2024-03-22 12:31:25 +0300

    delivery component added

---
+++ src/components/DeliveryComponent.vue
@@ -0,0 +1,71 @@
+<template>
+    <section class="delivery container">
+        <div class="delivery-left">
+            <img src="@/assets/images/componentsImg/DeliveryIcon.jpg" alt="delivery picture">
+        </div>
+        <div class="delivery-right">
+                <p class="text">Для наших покупателей доступны 2 способа доставки: курьерская доставка по Москве в пределах МКАД и самовывоз.</p>       
+            
+                <h3 class="title">Доставка</h3>
+                <p class="small-text">Ежедневно с 11:00 до 21:00.  Если Вы готовы принять заказ в интервале с 12 до 17 часов или с 17 до 21 часа, то доставка будет стоить 500 рублей.</p> 
+                <p class="small-text">При заказе от 3000 рублей доставка БЕСПЛАТНАЯ.</p>
+                <p class="small-text">Курьер предупредит Вас о своём прибытии за 30-40 минут.</p>
+                
+                <h3 class="title">Самовывоз</h3>
+                <p class="small-text">Вы можете забрать заказ самостоятельно с 10:00 до 20:00 по адресу: Москва, Цветной бульвар</p>
+        </div>
+    </section>
+    <section class="payment container">
+        <div class="payment-left">
+            <h3 class="title">Оплата</h3>
+            <p class="small-text">Вы можете оплатить заказ при получении наличными или банковской картой. Для этого укажите выбранный способ при оформлении заказа.</p>
+        </div>
+        <div class="payment-right">
+            <img src="@/assets/images/componentsImg/payment.png" alt="payment picture">
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/style.scss';
+.delivery {
+    @include flexAlignment(normal);
+    &-left {
+        width: 40%;
+        & img {
+            width: 100%;
+        }
+    }
+    &-right {
+        width: 60%;
+        padding: 65px 95px;
+    }
+}
+
+.payment {
+    @include flexAlignment(normal);
+    margin-bottom: 25px;
+    &-left {
+        width: 60%;
+        padding: 100px 130px;        
+    }
+    &-right {
+        width: 40%;
+        & img {
+            width: 75%;
+        }
+    }
+}
+
+
+.title {
+    margin-top: 15px;
+}
+
+</style>
--- src/views/DeliveryView.vue
+++ src/views/DeliveryView.vue
@@ -1,15 +1,27 @@
 <template>
-    <div>
+    <BannerComponent heading='Доставка и оплата' :class="'delivery-banner-bgImg'" :isBreadCrumbs='true'/>
 
-    </div>
+    <DeliveryComponent />
+    <FooterComponent />
 </template>
 
 <script>
+import BannerComponent from '@/components/BannerComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
+import DeliveryComponent from '@/components/DeliveryComponent.vue';
+
     export default {
-        
+        components: {
+            FooterComponent,
+            BannerComponent,
+            DeliveryComponent
+        },
     }
+
 </script>
-
-<style scoped>
-
+<style scoped lang="scss">
+@import '@/assets/style.scss';
+.delivery-banner-bgImg {
+  background-image: url('@/assets/images/componentsImg/BGDelivery.jpg');
+ }
 </style>
