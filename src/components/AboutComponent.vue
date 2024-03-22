16f896e216b933dce6c3fb2d033d0d0e1e6d1c3b
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2024-03-22 13:42:13 +0300

    About page has been created

---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="about">
+        <div class="about-content container">
+            <div class="heading">
+            <img src="@/assets/images/componentsImg/Logo.png" alt="logotype image"/>
+            <h1 class="brand-heading">Mary Candy Bar</h1>
+        </div>
+        <div class="description">
+            <p class="text">Мы готовим наши сладости с 2018 года. На протяжении 5 лет мы отбирали лучшие рецепты и технологии приготовления. И сегодня мы смело можем предложить вам наши изделия из шоколада. Уверены что они не оставят вас равнодушными. Мы много внимания уделяем мелочам, в том числе в производстве и на стадии отбора сырья и ингредиентов. Нашу продукцию часто покупают в подарок, поэтому мы всегда заботимся о том, чтобы вместе с ней человек получал эмоции. Это эмоции от упаковки, исполнения, ну и, конечно же, от вкуса.</p>
+        </div>
+        </div>
+        <img class="choco-img" src="@/assets/images/componentsImg/choco.png" alt="chokolate decorashion">
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped lang="scss">
+.heading {
+    text-align: center;
+    margin: 60px 0;
+}
+.description {
+    padding: 0 60px;
+    text-align: center;
+}
+
+.choco-img {
+    object-fit: cover;
+    width: 100%;
+    margin-top: -220px;
+    margin-bottom: -6px;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <BannerComponent heading='О нас' :class="'about-banner-bgImg'" :isBreadCrumbs='true'/>
+
+  <AboutComponent />
+
+  <FooterComponent />
 </template>
+
+
+<script>
+import BannerComponent from '@/components/BannerComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
+import AboutComponent from '@/components/AboutComponent.vue';
+
+
+    export default {
+        components: {
+            FooterComponent,
+            BannerComponent,
+            AboutComponent
+        },
+    }
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/style.scss';
+.about-banner-bgImg {
+  background-image: url('@/assets/images/componentsImg/BGimg.jpg');
+ }
+</style>
