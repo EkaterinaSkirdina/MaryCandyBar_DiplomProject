5bba1f2d77b8f6c6db0a8b9cc9f2a16361bed46d
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2024-03-24 17:54:16 +0300

    intermediate saving, a filteredList() method  in a component

---
+++ src/components/CardProductListComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="product-list container">
+        <CardProductComponent v-for="product in filteredList()" :key="product.id" :product = "product" />
+    </section>
+</template>
+
+<script>
+import CardProductComponent from '@/components/CardProductComponent.vue';
+
+import { mapState, mapMutations, mapGetters, mapActions} from 'vuex';
+
+    export default {
+        components: {
+          CardProductComponent
+      },
+
+      computed: {
+        ...mapGetters(['GET_PRODUCTS_LIST']),
+      },
+      methods: {
+        filteredList() {
+            const currentPage = String(this.$route.name);
+            console.log(currentPage);
+            return this.GET_PRODUCTS_LIST.filter(el => el.category === currentPage);
+             
+        }
+      }
+    }
+</script>
+
+<style scoped lang="scss">
+.product-list {
+    display: flex;
+    margin: 75px 0;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    gap: 15px;
+}
+</style>
