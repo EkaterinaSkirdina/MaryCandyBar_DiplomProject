c77978a1cbd95a2ce2e357c442472ef7458a8db3
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2024-04-03 12:56:04 +0300

    sweets-set view added

---
+++ src/views/SweetsSetView.vue
@@ -0,0 +1,30 @@
+<template>
+    <BannerComponent heading='Наборы' :class="'sweetsSet-banner-bgImg'" :isBreadCrumbs='true'/>
+
+    <CardProductListComponent />
+
+    <FooterComponent />
+</template>
+
+<script>
+import BannerComponent from '@/components/BannerComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
+import CardProductListComponent from '@/components/CardProductListComponent.vue';
+
+
+export default {
+    name: 'SweetsSetView',
+    components: {
+        FooterComponent,
+        BannerComponent,
+        CardProductListComponent,
+}
+}
+</script>
+
+<style scoped lang="scss">
+.sweetsSet-banner-bgImg {
+  background-image: url('@/assets/images/componentsImg/BGSweetsSet.jpg');
+  object-fit: cover;
+ }
+</style>
