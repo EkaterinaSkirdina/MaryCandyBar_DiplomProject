89117c91179163aafe737a4fffdc229b07fc35fb
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2024-04-03 12:09:35 +0300

    sweet view added

---
+++ src/views/SweetsView.vue
@@ -0,0 +1,29 @@
+<template>
+    <BannerComponent heading='Конфеты' :class="'sweets-banner-bgImg'" :isBreadCrumbs='true'/>
+
+    <CardProductListComponent />
+
+    <FooterComponent />
+</template>
+
+<script>
+import BannerComponent from '@/components/BannerComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
+import CardProductListComponent from '@/components/CardProductListComponent.vue';
+
+    export default {
+        name: 'SweetsView',
+        components: {
+            FooterComponent,
+            BannerComponent,
+            CardProductListComponent
+  }
+    }
+</script>
+
+<style scoped lang="scss">
+.sweets-banner-bgImg {
+  background-image: url('@/assets/images/componentsImg/BGSweets.jpg');
+  object-fit: cover;
+ }
+</style>
